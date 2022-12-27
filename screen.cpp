894c1edbd72f1dff8423a588d1fddae694b53cca
Marek528 <golias.marek.06@gmail.com>
2022-12-27 21:15:39 +0100

    uprava obrazovky

--- screen.cpp
+++ screen.cpp
@@ -1,43 +1,90 @@
 #include <iostream>
 #include <windows.h>
-#include <functions.cpp>
-#include <libraries/termcolor.hpp>
+#include "functions.cpp"
+#include "libraries/termcolor.hpp"
 
 #include <io.h>
 #include <fcntl.h>
 
 using namespace std;
 
-void vypis_listy(wstring x, int row)
+void vypis_listy(wstring x, wstring y, int row)
 {
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 15; i++)
     {
-        gotoxy(53+i, row);
+        gotoxy(56+i, row);
         wcout << x;
     }
+
+    gotoxy(71, row);
+    wcout << y;
+
+    for (int i = 0; i < 15; i++)
+    {
+        gotoxy(72 + i, row);
+        wcout << x;
+    }
+
+    gotoxy(87, row);
+    wcout << y;
+
+    for (int i = 0; i < 15; i++)
+    {
+        gotoxy(88+i, row);
+        wcout << x;
+    }
+}
+
+void riadok(wstring start, wstring end, int row, wstring con)
+{
+    wstring lista = L"\x2550";
+
+    gotoxy(55, row);
+    wcout << start;
+    vypis_listy(lista, con, row);
+    gotoxy(103, row);
+    wcout << end << endl;
 }
 
 void screen()
 {
     _setmode(_fileno(stdout), _O_U16TEXT);
     wstring lista = L"\x2550";
+    wstring pole = L"\x2551               \x2551               \x2551               \x2551";
+    wstring krizik = L"\x256C"; // this
+    wstring leftTop = L"\x2554";
+    wstring rightTop = L"\x2557";
+    wstring conMidStart = L"\x2560";
+    wstring conMidEnd = L"\x2563";
+    wstring conMidTop = L"\x2566"; // this
+    wstring conMidBot = L"\x2569"; // this
+    wstring leftBot = L"\x255A";
+    wstring rightBot = L"\x255D";
 
     cout << termcolor::yellow << endl;
-    gotoxy(52, 1);
-    wcout << L"\x2554";
-    vypis_listy(lista, 1);
-    gotoxy(103, 1);
-    wcout << L"\x2557" << endl;
-    for (int i = 0; i < 12; i++)
+
+    riadok(leftTop, rightTop, 1, conMidTop);
+
+    for (int i = 0; i < 4; i++)
     {
-        gotoxy(52, 2+i);
-        wcout << L"\x2551                                                  \x2551" << endl;
+        gotoxy(55, 2+i);
+        wcout << pole << endl;
     }
-    gotoxy(52, 14);
-    wcout << L"\x255A";
-    vypis_listy(lista, 14);
-    gotoxy(103, 14);
-    wcout << L"\x255D" << endl;
+
+    riadok(conMidStart, conMidEnd, 6, krizik);
+
+    gotoxy(55, 7);
+    wcout << pole << endl;
+
+    riadok(conMidStart, conMidEnd, 8, krizik);
+
+    for (int i = 0; i < 4; i++)
+    {
+        gotoxy(55, 9+i);
+        wcout << pole << endl;
+    }
+
+    riadok(leftBot, rightBot, 13, conMidBot);
     cout << termcolor::white << endl;
 }
 
